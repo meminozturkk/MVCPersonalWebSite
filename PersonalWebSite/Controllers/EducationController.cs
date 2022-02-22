8186fe84bc71bce63acf7bcc40bef6b69c75f7b8
Muhammet Emin Öztürk <m.eminoztrk@outlook.com>
2022-02-22 13:47:03 +0300

    3. kayit

--- PersonalWebSite/Controllers/EducationController.cs
+++ PersonalWebSite/Controllers/EducationController.cs
@@ -26,13 +26,44 @@ namespace PersonalWebSite.Controllers
             return RedirectToAction("Index");
         }
 
-        [HttpPost]
-        public ActionResult NewEducation(TblEducation education)
+        [HttpGet]
+        public ActionResult AddEducation()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddEducation(TblEducation education)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("AddEducation");
+            }
             repository.TAdd(education);
             return RedirectToAction("Index");
         }
 
+        [HttpGet]
+        public ActionResult GetEducation(int id)
+        {
+            TblEducation education = repository.Find(x => x.Id == id);
+            return View(education);
+        }
+        
+        [HttpPost]
+        public ActionResult GetEducation(TblEducation e)
+        {
+            var education = repository.Find(x => x.Id == e.Id);
+            education.Title = e.Title;
+            education.Subtitle1 = e.Subtitle1;
+            education.Subtitle2 = e.Subtitle2;
+            education.GNO = e.GNO;
+            education.Date = e.Date;
 
+            repository.TUpdate(education);
+
+            return RedirectToAction("Index");
+        }
+ 
     }
 }
