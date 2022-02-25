268e36b8405812dc092a2aed33e6fca3950896af
Muhammet Emin Öztürk <m.eminoztrk@outlook.com>
2022-02-25 12:56:17 +0300

    6. kayıt

--- PersonalWebSite/Controllers/CertificateController.cs
+++ PersonalWebSite/Controllers/CertificateController.cs
@@ -3,15 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PersonalWebSite.Models.Entity;
+using PersonalWebSite.Repository;
+using PersonalWebSite.Controllers;
 
 namespace PersonalWebSite.Controllers
 {
     public class CertificateController : Controller
     {
         // GET: Certificate
+        GenericRepository<TblCertificate> reporsitory = new GenericRepository<TblCertificate>();
         public ActionResult Index()
         {
-            return View();
+            var certificate = reporsitory.List();
+            return View(certificate);
+        }
+
+        public ActionResult EditCerficate()
+        {
+
         }
     }
 }
